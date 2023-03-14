5a7ff7a8f5e79f1b0c02374dfdeba1fe3a1f9788
Bangeunseong <bangeunseong@naver.com>
2023-03-14 09:54:32 +0900

    Commit

---
+++ Practice_1/BinarySearch/CuttingWood.c
@@ -0,0 +1,27 @@
+//BinarySearch 2805
+
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning(disable:4996)
+#define SIZE 1000000
+
+long arr[SIZE]; int n, k;
+
+long Count(int len){
+    long sum = 0;
+    for(int i = 0; i < n; i++){
+        sum += arr[i] - len > 0 ? arr[i] - len : 0;
+    }
+    return sum;
+}
+
+int main(){
+    scanf("%d%d",&n,&k);
+
+    long maxLength = 0;
+    for(int i = 0; i < n; i++){
+        scanf("%ld", &arr[i]);
+        if(i == 0) maxLength = arr[i];
+        else maxLength = maxLength < arr[i] ? arr[i] : maxLength;
+    }
+}
