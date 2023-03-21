29102fa3a048594bfb612a39c0030c16341a2f40
Bangeunseong <bangeunseong@naver.com>
2023-03-21 09:56:11 +0900

    Commit

---
+++ Practice_1_BaekJoon/By_Category/Mathmatics/Division.c
@@ -0,0 +1,15 @@
+//Mathematics 2225
+
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning(disable:4996)
+#define NUM 1000000000
+
+int n, k;
+long Calculate(){
+    if(k == 2) return (n + 1) % NUM;
+    
+}
+int main(){
+    scanf("%d%d",&n,&k);
+}
